384d196e36c55c66e109cf698c1ebf1eb8846859
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2021-08-02 21:13:57 +0200

    Create es1.c

---
+++ es1.c
@@ -0,0 +1,20 @@
+/**
+ * 
+ * esercizio:
+ * 
+ * INPUT:
+ *  numero intero positivo da linea di comando
+ * OUTPUT:
+ *  i numeri da 0 al numero dato in input e la loro somma
+ * 
+ * ESEMPIO:
+ * 
+ *  Inserisci un numero: <4 (inserito da tastiera)>
+ *  0
+ *  1
+ *  2
+ *  3
+ *  4
+ *  la somma di questi 5 numeri è 10
+ * 
+ * */
