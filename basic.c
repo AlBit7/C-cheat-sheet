d831e7dd7670d817adc0c4daee1587812d0eb5d2
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2021-08-02 21:13:05 +0200

    Create basic.c

---
+++ basic.c
@@ -0,0 +1,134 @@
+#include <stdio.h>          // capia e incolla il contenuto del file stdio.h --> standard input output . header
+#include <math.h>    
+
+#define NOME_MAIN main()    // copia e incolla il valore dovunque si presenta il nome nel programma
+
+int NOME_MAIN {
+
+    int num;                                    // numero intero 8/16 bit (dipende)
+    const float numeroDecimale = 3.5;           // numero decimale costante (solo leggibile) de 32 bit
+    double numeroMoltoLungo = 87954126875623;   // numero decimale de 64 bit
+    char carattere = 'r';                       // carattere da 4 bit
+
+    printf("inserisci u\tn numero: \n");
+    scanf("%d", &num);
+
+    printf("il numero e': %d %.2f %.0lf %c\n", num, numeroDecimale, numeroMoltoLungo, carattere);
+
+    int a = 4;
+    int b = 5;
+    int tmp;
+
+    printf("%d %d\n", a, b);
+
+    tmp = a;
+    a = b;
+    b = tmp;
+
+    printf("%d %d\n", a, b);
+
+    /*
+    
+    OPERATORI MATEMATICE PER LE VARIABILI
+
+    a++;
+    ++a;
+    a--;
+    --a;
+    a += 2; --> a = a + 2
+    a -= 2;
+    a /= 2;
+    a *= 2;
+    
+    */
+    
+    /*
+    
+    CONDIZIONI
+
+    >
+    <
+    ==
+    !=
+
+    OPERATORI LOGICI
+
+    && and   && > || tipo * > +
+    || or
+    !  not
+    ^  xor
+    
+    (valopre <CONDIZIONE> valore) <OPERATORE> (valopre <CONDIZIONE> valore) <OERATORE> valore
+
+    */
+
+    #ifndef NOME_MAIN
+    if (/* condizione */) {
+        // ...
+    } else if () {
+        // ...
+    } else {
+        // ...
+    }
+
+    switch (/* variabile / espressione */) {
+    
+    case 1 /* il valore dell'espressione è 1 */:
+        /* code */
+        break;
+    
+    case 2 /* il valore dell'espressione è 2 */:
+        /* code */
+        break;
+
+    default:
+        // se l'espressione non corrisponde a nessun case allora..
+        // esegui questo codice
+        break;
+    
+    }
+
+    for ( /* dichiarazzione */; /* condizione */; /* modifica (incremento / decremento) */) {
+        // ...
+    }
+
+    do
+    {
+        /* code */
+    } while (/* condition */);
+
+    while (/* condition */)
+    {
+        /* code */
+    }
+
+    #endif
+
+    for (unsigned char i = 0; i < 255; ++i) {
+        // i = [0; 254] - eseguito 255 volte
+
+        printf("%d\t-->\t%c\n", i, i);
+
+    }
+
+    unsigned char i = 0;
+    while (i < 255)
+    {
+        ++i;
+
+        printf("%d\t-->\t%c\n", i, i);
+        
+    }
+    
+
+    // --------------------- funzioni matematiche ---------------------
+
+    // importare #include <math.h> per utilizzarle
+
+    // seno = sin(angoloInRadianti)
+    // radice = sqrt(radicando)
+    // potenza = pow(base, esponente)
+    // potenzaDiE = exp(potenza)
+    // logaritmo = log(base, argomento)
+
+}
