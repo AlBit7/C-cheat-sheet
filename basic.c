eab6617f3050409386286f372a503eec016befaf
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2022-02-27 10:05:04 +0100

    Update basic.c

--- basic.c
+++ basic.c
@@ -50,7 +50,8 @@ int NOME_MAIN {
     <
     ==
     !=
-
+    !(condizione) --> non 
+    
     OPERATORI LOGICI
 
     && and   && > || tipo * > +
@@ -60,6 +61,38 @@ int NOME_MAIN {
     
     (valopre <CONDIZIONE> valore) <OPERATORE> (valopre <CONDIZIONE> valore) <OERATORE> valore
 
+    esempi con l'AND
+    (1 > 2) && (1 == 1) --> falso e vero --> falso
+    !(1 > 2) && (1 == 1) --> vero e vero --> vero
+    
+    esempi con l'OR
+    (1 > 2) || (1 == 1) --> falso o vero --> vero
+    (1 > 2) || (1 != 1) --> falso o falso --> falso
+    
+    A = 2
+    B = 4
+    C = 7
+    
+    (C > A) && (C > B) --> vero e vero --> vero
+    
+    tabelle di condizione:
+    
+    OR: (||)
+    vero || vero   --> vero
+    vero || falso  --> vero
+    falso || vero  --> vero
+    falso || falso --> falso 
+    
+    AND: (&&)
+    vero && vero   --> vero
+    vero && falso  --> falso
+    falso && vero  --> falso
+    falso && falso --> falso 
+    
+    NOT: (!)
+    !vero --> falso 
+    !falso --> vero
+    
     */
 
     #ifndef NOME_MAIN
